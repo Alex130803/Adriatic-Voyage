c513f60787be621558daf2f20cf9816af62854ff
Alex130803 <ai3522@g.rit.edu>
2025-05-01 12:03:47 +0200

    Begaa

---
+++ scripts/script.js
@@ -0,0 +1,73 @@
+// POPUP
+function openPopup() {
+    document.getElementById("popup").style.display = "flex";
+}
+  
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+  
+// FLATPICKR
+document.addEventListener("DOMContentLoaded", function () {
+    flatpickr("#date", {
+      dateFormat: "d.m.Y",
+      altInput: true,
+      altFormat: "F j, Y"
+    });
+});
+
+flatpickr("#quick-date-2", {
+    dateFormat: "d.m.Y",
+    altInput: true,
+    altFormat: "F j, Y"
+  });
+  
+  function showThanks(e) {
+    e.preventDefault();
+    document.getElementById("thanks-msg").classList.add("visible");
+  }
+
+  //Za abour us sekciju
+  const steps = document.querySelectorAll('.about-snap .step');
+  const aboutSection = document.querySelector('.about-lock');
+  let currentStep = 0;
+  let locked = false;
+  let observerStarted = false;
+  
+  function lockScroll(e) {
+    if (locked) e.preventDefault();
+  }
+  
+  function handleStepScroll(e) {
+    if (!locked) return;
+  
+    if (e.deltaY > 0 && currentStep < steps.length) {
+      steps[currentStep].classList.add('revealed');
+      currentStep++;
+  
+      if (currentStep === steps.length) {
+        setTimeout(() => {
+          locked = false;
+          window.removeEventListener('wheel', lockScroll);
+          window.removeEventListener('touchmove', lockScroll);
+          aboutSection.style.height = 'auto';
+        }, 600);
+      }
+    }
+  }
+  
+  // Observer da pokrene animaciju tek kad se sekcija vidi
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && !observerStarted) {
+        locked = true;
+        observerStarted = true;
+        window.addEventListener('wheel', lockScroll, { passive: false });
+        window.addEventListener('touchmove', lockScroll, { passive: false });
+        window.addEventListener('wheel', handleStepScroll, { passive: false });
+      }
+    });
+  }, { threshold: 0.6 });
+  
+  observer.observe(aboutSection);
+  
